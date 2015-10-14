0d7f09fed099d88ec346776e6bead0a08f009818
Avi Yashchin <avi.yashchin@gmail.com>
2015-10-14 19:08:26 -0400

    deleting everything except MrT

--- AshlyMadison/big-dog-master/functions/Marginals.r
+++
@@ -1,7 +0,0 @@
-
-Marginals <- function(data){
-	require(ggplot2)
-		
-	p <- ggplot(data, aes_q(x = as.name(name))) + geom_density()		
-
-}
--- AshlyMadison/big-dog-master/public/js/controllers/events.js
+++
@@ -1,7 +0,0 @@
-function dragOver(event) {
-	event.target.style.border = "7px solid black";
-}
-
-function dragLeave(event) {
-	event.target.style.border = "7px dashed white";
-}
