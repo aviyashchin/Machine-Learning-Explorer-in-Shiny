44bd39f96018763e2f7a3dfa57ecc041a5f28661
Avi Yashchin <avi.yashchin@gmail.com>
2015-11-24 21:23:11 -0500

    initial commit

--- archive/source/load_DataSets.R
+++
@@ -1,4 +0,0 @@
-load_DataSets <- function(){
-  titanic <<- titanic3
-  airquality <<- airquality
-}
--- archive/source/runSQL.R
+++
@@ -1,8 +0,0 @@
-runSQL <- function(con,Query){
-
-  print(paste("Calling SQL Query:",Query))
-  rs <- dbSendQuery(con, Query)
-  tempsql <- fetch(rs,n=-1)
-  dbClearResult(rs)
-  return(tempsql);
-}
